18e8bcc06ccf0747a971f793f2c1aeb4fefb2859
Hassan Raja <hassan.uog001@gmail.com>
2020-07-03 11:08:51 +0500

    Updates

---
+++ precache-manifest.3e0308df6eb6e6a8c5c55787cfccd23e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "643c83c9f1d52df4068c2dd2a0d6bbba",
+    "url": "/book-finder-app/index.html"
+  },
+  {
+    "revision": "753dd75657ef66ac99eb",
+    "url": "/book-finder-app/static/css/main.ee080cdc.chunk.css"
+  },
+  {
+    "revision": "398ff38529b73aee7ae5",
+    "url": "/book-finder-app/static/js/2.e0d67c37.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/book-finder-app/static/js/2.e0d67c37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "753dd75657ef66ac99eb",
+    "url": "/book-finder-app/static/js/main.78ffddb9.chunk.js"
+  },
+  {
+    "revision": "522115bbfa4d94499caa",
+    "url": "/book-finder-app/static/js/runtime-main.a2791a58.js"
+  }
+]);
